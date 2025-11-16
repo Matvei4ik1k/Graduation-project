b73444f7a2ad6cc862aa7157f04601d001e0e4d2
Matvei4ik1k <motamota6937@gmail.com>
2025-11-16 18:33:26 +0300

    Добавьте файлы проекта.

---
+++ Graduation project/View/Windows/AuthorizationWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using MahApps.Metro.Controls;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Graduation_project.View.Windows
+{
+    /// <summary>
+    /// Логика взаимодействия для AuthorizationWindow.xaml
+    /// </summary>
+    public partial class AuthorizationWindow : MetroWindow
+    {
+        public AuthorizationWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void LoginBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void RegistrationBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
