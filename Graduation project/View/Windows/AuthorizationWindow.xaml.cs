a635fae061fdba92b620fcabe2ebd5121855ed1c
1 <1@HOME-PC>
2026-03-06 14:38:58 +0300

    ...

--- Graduation project/View/Windows/AuthorizationWindow.xaml.cs
+++ Graduation project/View/Windows/AuthorizationWindow.xaml.cs
@@ -1,5 +1,4 @@
-﻿using MahApps.Metro.Controls;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -36,5 +35,7 @@ RegistrationWindow registration = new RegistrationWindow();
             registration.Show();
             this.Close();   
         }
+
+       
     }
 }
